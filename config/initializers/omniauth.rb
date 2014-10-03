943cee3464ef9afb3b18db7f4d62236d705e0ce4
iberianpig <yhkyky@gmail.com>
2014-10-03 12:45:26 +0900

    add jumbotron

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_CONSUMER_KEY'], ENV['FB_CONSUMER_SECRET']
+  provider :facebook, ENV['FB_CONSUMER_KEY'], ENV['FB_CONSUMER_SECRET'],
+    scope: 'email, user_friends'
 end
